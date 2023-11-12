f7a07b8f0d4902c474de676a68f2a27a5d46e9dc
push-ebx <nikita.keyn@yandex.ru>
2023-11-12 21:47:00 +0300

    fix readme

---
+++ index.js
@@ -0,0 +1,29 @@
+const drop_area = document.getElementsByClassName('card')[0];
+const save_button = document.getElementsByClassName('btn-success')[0];
+const load_button = document.getElementsByClassName('btn-info')[0];
+save_button.style.display = 'none';
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  drop_area.addEventListener(eventName, (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, false)
+});
+
+drop_area.addEventListener('drop', (e) => {
+  document.getElementById('file').files = e.dataTransfer.files;
+  save_button.style.display = 'block';
+  load_button.text = e.dataTransfer.files[0].name;
+});
+
+['dragenter', 'dragover'].forEach(eventName => {
+  drop_area.addEventListener(eventName, _ => {
+    drop_area.style.cssText = 'box-shadow: 0 0 .5rem rgba(0,250,0,.75) !important;'
+  }, false)
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  drop_area.addEventListener(eventName, _ => {
+    drop_area.style.cssText = 'box-shadow: var(--suai-shadow) !important'
+  }, false)
+});
